3eccdb51876014b7182822043c55d3f5e0beffb4
David Cao <sirdavidcao@gmail.com>
2024-06-28 06:18:16 -0600

    preview links for expo

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,6 @@
-import {View} from '@/components/Themed';
-import {Text} from '@/components/Themed';
-import {signOut} from '@/lib/Auth';
 import {useAuth} from '@/providers/AuthProvider';
-import {Link, Redirect} from 'expo-router';
-import {ActivityIndicator, Button} from 'react-native';
+import {Redirect} from 'expo-router';
+import {ActivityIndicator} from 'react-native';
 
 export default function IndexPage() {
   const {session, loading} = useAuth();
