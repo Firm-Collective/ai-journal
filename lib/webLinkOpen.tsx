2279e3c49c28e52a6aa72015304e27fc965e6730
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-07-08 16:53:19 -0700

    added an auth link opening function
    Signed-off-by: Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>

---
+++ lib/webLinkOpen.tsx
@@ -0,0 +1,19 @@
+import * as WebBrowser from 'expo-web-browser';
+import {Platform} from 'react-native';
+
+export default async function openAuthLink(link) {
+  console.log('Link in open func ', link);
+
+  const platformAppLink =
+    Platform.OS === 'android' ? 'com.firmcollective.OneVoiceEcho' : 'none';
+
+  try {
+    const result = await WebBrowser.openAuthSessionAsync(link, platformAppLink);
+    console.log('Auth state is ', result.type);
+    console.log('Auth Session Result:', result);
+    return result;
+  } catch (error) {
+    console.error('Error opening auth link:', error);
+    throw error;
+  }
+}
