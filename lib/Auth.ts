d852006b69f6a5f2db82abf63dec3ffce6c12b6a
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-07-09 16:56:08 -0700

    Added apple login logic
    Signed-off-by: Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>

--- lib/Auth.ts
+++ lib/Auth.ts
@@ -1,7 +1,8 @@
-import {Alert} from 'react-native';
+import {Alert, Platform} from 'react-native';
 import {supabase} from './supabase';
 import openAuthLink from '@/lib/webLinkOpen';
 import {makeRedirectUri} from 'expo-auth-session';
+import * as AppleAuthentication from 'expo-apple-authentication';
 
 /**
  * Signs up the user with an email and password
@@ -83,6 +84,88 @@ export async function signInWithFacebook() {
     //   toDo: handle case of other error
   }
 }
+
+export async function signInWithApple() {
+  if (Platform.OS === 'ios') {
+    await signInWithAppleNative();
+  } else if (Platform.OS === 'android') {
+    await signInWithAppleAndroid();
+  }
+}
+
+const signInWithAppleNative = async () => {
+  try {
+    const credential = await AppleAuthentication.signInAsync({
+      requestedScopes: [
+        AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+        AppleAuthentication.AppleAuthenticationScope.EMAIL,
+      ],
+    });
+    // Sign in via Supabase Auth.
+    if (credential.identityToken) {
+      const {
+        error,
+        data: {user},
+      } = await supabase.auth.signInWithIdToken({
+        provider: 'apple',
+        token: credential.identityToken,
+      });
+      console.log(JSON.stringify({error, user}, null, 2));
+      if (!error) {
+        // User is signed in.
+        // toDo: handle user onboarding logic
+        console.log('User signed in');
+      }
+    } else {
+      console.log('No id token');
+      throw new Error('No identityToken.');
+    }
+  } catch (e) {
+    if (e.code === 'ERR_REQUEST_CANCELED') {
+      // handle that the user canceled the sign-in flow
+      console.log('flow canceled');
+    } else {
+      // handle other errors
+      console.log('Misc errors occurred');
+    }
+  }
+};
+
+const signInWithAppleAndroid = async () => {
+  const redirectOnAuthLocation = makeRedirectUri();
+  console.log('We will redirect to ', redirectOnAuthLocation);
+
+  const {
+    data: {url: supabaseAppleUrl},
+    error: supabaseError,
+  } = await supabase.auth.signInWithOAuth({
+    provider: 'apple',
+    options: {
+      redirectTo: redirectOnAuthLocation,
+    },
+  });
+
+  if (supabaseAppleUrl) {
+    const authResult = await openAuthLink(supabaseAppleUrl);
+    // If the user does not permit the application to authenticate with the given url, the Promise fulfills with { type: 'cancel' } object.
+    // If the user closed the web browser, the Promise fulfills with { type: 'cancel' } object.
+    // If the browser is closed using dismissBrowser, the Promise fulfills with { type: 'dismiss' } object.
+
+    if (authResult.type === 'cancel') {
+      console.log('Browser event canceled');
+      //   toDo: handle cancel -- some sort of error
+    } else if (authResult.type === 'dismiss') {
+      //   toDo: handle dismiss --
+      console.log('Browser dismissed');
+    } else if (authResult.type === 'success') {
+      //   toDo: handle success
+      console.log('Browser event success');
+    }
+  } else if (supabaseError) console.log('An error occurred with supabase');
+  else {
+    //   toDo: handle case of other error
+  }
+};
 
 export async function signOut() {
   const {error} = await supabase.auth.signOut();
