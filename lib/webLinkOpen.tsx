577f67167a818be1e0014d13dc82e9b6153eded9
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-07-08 19:22:55 -0700

    redirect logic 20%
    Signed-off-by: Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>

--- lib/webLinkOpen.tsx
+++ lib/webLinkOpen.tsx
@@ -1,5 +1,6 @@
 import * as WebBrowser from 'expo-web-browser';
 import {Platform} from 'react-native';
+import * as Linking from 'expo-linking';
 
 export default async function openAuthLink(link: string) {
   console.log('Link in open func ', link);
@@ -9,8 +10,20 @@ export default async function openAuthLink(link: string) {
 
   try {
     const result = await WebBrowser.openAuthSessionAsync(link, platformAppLink);
+
     console.log('Auth state is ', result.type);
     console.log('Auth Session Result:', result);
+
+    const loginUrl = Linking.createURL('/login');
+
+    console.log('Url is ', loginUrl);
+
+    if (result.type === 'success') {
+      const loginUrl = Linking.createURL('/login');
+      if (await Linking.canOpenURL(loginUrl)) await Linking.openURL(loginUrl);
+      else console.log('We cannot open that lol');
+    }
+
     return result;
   } catch (error) {
     console.error('Error opening auth link:', error);
