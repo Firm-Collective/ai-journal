dbcdd17bb93e1d6e0723d07ce82971b8ad340545
Julian Beaudry <julian_beaudry@brown.edu>
2024-07-10 17:29:53 -0400

    done

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -31,10 +31,10 @@ const CompressedSecureStore = {
 
 const supabaseUrl =
   process.env.EXPO_PUBLIC_SUPABASE_URL ||
-  'https://udvardvrnqppvvvfqaac.supabase.co';
+  '';
 const supabaseAnonKey =
   process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ||
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkdmFyZHZybnFwcHZ2dmZxYWFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkwMDg0NTYsImV4cCI6MjAzNDU4NDQ1Nn0.EuY_I4seWBcAScl_vlOS6THNgAw77VmZKO6phv9qUt0';
+  '';
 
 // TODO: storage option to store supabase cookie
 
