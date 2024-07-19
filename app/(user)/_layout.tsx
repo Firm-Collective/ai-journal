71c3998cf27b48c21e729720376a4dfe2895db94
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-07-19 00:22:53 -0700

    fix: needed imports
    Signed-off-by: Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>

--- app/(user)/_layout.tsx
+++ app/(user)/_layout.tsx
@@ -6,6 +6,7 @@ import Colors from '@/constants/Colors';
 import {useColorScheme} from '@/components/useColorScheme';
 import {useClientOnlyValue} from '@/components/useClientOnlyValue';
 import {useAuth} from '@/providers/AuthProvider';
+import React from 'react';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
