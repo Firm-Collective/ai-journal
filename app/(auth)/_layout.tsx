d6f8ea26c0a7cba77a5a1b40f67f92cf039d0ece
Nankun-Xu <113734143+Nankun-Xu@users.noreply.github.com>
2024-07-23 19:07:26 -0700

    route to tell-us screen after 1st login-worked on email login

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -2,12 +2,12 @@ import {useAuth} from '@/providers/AuthProvider';
 import {Redirect, Stack} from 'expo-router';
 
 export default function AuthLayout() {
-  const {session} = useAuth();
+  // const {session} = useAuth();
 
-  // if user is logged in, redirect to home screen
-  if (session) {
-    return <Redirect href={'/'} />;
-  }
+  // // if user is logged in, redirect to home screen
+  // if (session) {
+  //   return <Redirect href={'/'} />;
+  // }
 
   return (
     <Stack>
