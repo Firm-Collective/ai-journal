00e138123d94d9270bf640f8ceb03aaf3ca3b86b
David Cao <sirdavidcao@gmail.com>
2024-08-11 03:23:29 -0600

    got rid of header for home page

--- app/(user)/_layout.tsx
+++ app/(user)/_layout.tsx
@@ -14,7 +14,7 @@ export default function TabLayout() {
   return (
     <JournalEntriesProvider>
       <Stack>
-        <Stack.Screen name="home/index" options={{headerShown: true}} />
+        <Stack.Screen name="home/index" options={{headerShown: false}} />
         <Stack.Screen name="text-entry" options={{headerShown: true}} />
         <Stack.Screen name="edit/[id]" options={{headerShown: true}} />
         <Stack.Screen name="settings" options={{headerShown: true}} />
