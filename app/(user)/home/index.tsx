549bdb5d99c36ec903dbcc73fd0fd273fe4f77f7
Avina <mauryavina20@gmail.com>
2025-02-09 15:54:15 -0500

    Revert "Updated nav"

--- app/(user)/home/index.tsx
+++ app/(user)/home/index.tsx
@@ -1,10 +1,5 @@
-import { LayoutProvider } from '@/components/context/LayoutContext';
 import HomeScreen from '@/components/user/home-screen';
 
 export default function Page() {
-  return (
-    <LayoutProvider>
-      <HomeScreen />
-    </LayoutProvider>
-  );
+  return <HomeScreen />;
 }
